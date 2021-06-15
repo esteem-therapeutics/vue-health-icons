7259f406a2400bfd6ccbd025732ca096a2828f3a
Jamie McElwain <jgmcelwain@icloud.com>
2021-06-15 14:31:36 +0100

    feat: use vue-feather-icons props system

--- build.js
+++ build.js
@@ -13,11 +13,17 @@ export default {
   name: '${name}',
   functional: true,
   props: {
-    size: { type: Number, default: 24, required: false }
+    size: {
+      type: String,
+      default: '24',
+      validator: (s) => (!isNaN(s) || s.length >= 2 && !isNaN(s.slice(0, s.length -1)) && s.slice(-1) === 'x' )
+    }
   },
   render(h, ctx) {
-    const size = ctx.props.size + 'px';
-    
+    const size = ctx.props.size.slice(-1) === 'x' 
+      ? ctx.props.size.slice(0, ctx.props.size.length -1) + 'em'
+      : parseInt(ctx.props.size) + 'px';
+
     const attrs = ctx.data.attrs || {};
     attrs.width = attrs.width || size;
     attrs.height = attrs.height || size;
@@ -91,7 +97,7 @@ async function main() {
   const types = `import Vue from 'vue';
 import { ExtendedVue } from 'vue/types/vue';
 export interface HealthIconProps {
-  size: number;
+  size: string;
 }
 export type HealthIconComponent = ExtendedVue<
   Vue,
